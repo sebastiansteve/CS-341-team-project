494d002e2f46236a45b657ba05edc349b4fc8002
dreyvonn <38699352+dreyvonn@users.noreply.github.com>
2020-11-10 08:53:20 -0700

    tuesday team work

---
+++ routes/gallery.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const galleryController = require('../controllers/galleryControl.js');
+const adminController = require('../controllers/adminControl.js');
+const authController = require('../controllers/authControl');
+
+module.exports = router;
