526e3d1d82b6e26469a91da0d5fcb04f54237116
Teancum007 <icblamires@gmail.com>
2020-11-12 09:09:10 -0700

    Added standard logout function.

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -21,5 +21,8 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postLogout = (req, res, next) => {
-    res.redirect('/');
+    req.session.destroy(err => {
+      console.log(err);
+      res.redirect('/');
+    });
 };
